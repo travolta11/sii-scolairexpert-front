20a38a30f3046469c7bd48289ce3cf9688de22d5
issam liqali <63265720+travolta11@users.noreply.github.com>
2024-07-12 10:48:47 +0100

    configuration des routes

--- src/app/shared/page-content/page-content.component.ts
+++ src/app/shared/page-content/page-content.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+
 
 @Component({
   selector: 'ssi-sx-page-content',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './page-content.component.html',
   styleUrl: './page-content.component.scss'
 })
