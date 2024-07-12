a5dd84fe79a4f9ec508db12cce9a4b0a11cfb3cb
issam liqali <63265720+travolta11@users.noreply.github.com>
2024-07-12 15:56:38 +0100

    réalisation d'interface la liste de gestion des utilisateurs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageUserComponent } from './components/manage-user/manage-user.component';
 import { AuthGuard } from './guards/auth.guard';
 import { UnauthorizedComponent } from './components/unauthorized';
 
 export const routes: Routes = [
     { path: '', title: 'DashBoard', component: DashboardComponent, canActivate: [AuthGuard], data: { roles: ['admin'] } },
     { path: 'unauthorized', component: UnauthorizedComponent },
+    {'path': 'manage-user', 'title': 'User Management', component: ManageUserComponent}
     // Route example: { path: 'add-student', title: 'Add', component: AddStudentComponent },
     
 ];
