a834057ee07034e7a8266c5d3c3b22940e4dcdbf
ISSAM LIQALI <liqaliissam@gmail.com>
2024-07-29 09:41:40 +0000

    keycloak integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,30 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { KeycloakService } from './services/keycloak/keycloak.service';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { APP_INITIALIZER } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/core';
+import { AuthGuard } from './guards/auth.guard';
 import { provideClientHydration } from '@angular/platform-browser';
 
+export function kcFactory(kcService: KeycloakService) {
+  return () => kcService.init();
+}
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideClientHydration(),
+    AuthGuard,
+    KeycloakService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: kcFactory,
+      deps: [KeycloakService],
+      multi: true
+    }
+  ]
 };
