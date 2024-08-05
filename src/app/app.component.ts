97d01c318e8ecb4ac27d368d5718e5443bc4379e
issam liqali <63265720+travolta11@users.noreply.github.com>
2024-08-05 10:43:20 +0100

    implementation keycloak token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component  } from '@angular/core';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { PageContentComponent } from './shared/page-content/page-content.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { KeycloakService } from './services/keycloak/keycloak.service';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SidebarComponent, PageContentComponent, NavbarComponent , FooterComponent],
+  imports: [SidebarComponent, PageContentComponent,FooterComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'SSI-Ecole-Front';
+  
  
 }
