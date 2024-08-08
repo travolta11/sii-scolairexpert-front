9445cef70110d25c056f7dc29b694791969ed461
ISSAM LIQALI <liqaliissam@gmail.com>
2024-08-08 09:52:12 +0000

    User crud

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,19 +6,21 @@ import { AuthGuard } from './guards/auth.guard';
 import { UnauthorizedComponent } from './components/unauthorized';
 import { AddTeacherComponent } from './components/Teacher/add-teacher/add-teacher.component';
 import { ListTeacherComponent } from './components/Teacher/list-teacher/list-teacher.component';
-
+import { UserDetailComponent } from './components/user/show-user/show-user.component';
 
 export const routes: Routes = [
     { path: '', title: 'DashBoard', component: DashboardComponent, canActivate: [AuthGuard], data: { roles: ['admin'] } },
     { path: 'unauthorized', component: UnauthorizedComponent },
     {'path': 'manage-users', 'title': 'User Management', component: ManageUserComponent},
     {'path': 'add-user', 'title': 'Add User', component: AddUserComponent},
+    { path: 'manage-users/user-detail/:id', component: UserDetailComponent },
     // Route example: { path: 'add-student', title: 'Add', component: AddStudentComponent },
     
     {path:'addTeacher',component:AddTeacherComponent},
-    {path:'listTeacher',component:ListTeacherComponent}
+    {path:'listTeacher',component:ListTeacherComponent},
 
     //Route example {'path': 'add-student', 'title': 'add', component: AddStudentComponent},
+   
 
 ];
 
