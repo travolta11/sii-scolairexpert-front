8ae29352e00d9b6ecc41e1a79c058d54c6c9bd63
Abdessamad BARIZIZ <a.bariziz5294@uca.ac.ma>
2024-08-09 08:44:02 +0000

    Feat/86952f40z

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
 
 import { ManageUserComponent } from './components/user/manage-user/manage-user.component';
 import { AddUserComponent } from './components/user/add-user/add-user.component';
@@ -12,17 +13,16 @@ import {StaffListComponent} from "./components/staff/staff-list/staff-list.compo
 import { UserDetailComponent } from './components/user/show-user/show-user.component';
 
 export const routes: Routes = [
+    {'path': 'login',  component:LoginComponent },
     { path: '', title: 'DashBoard', component: DashboardComponent, canActivate: [AuthGuard], data: { roles: ['admin'] } },
     { path: 'unauthorized', component: UnauthorizedComponent },
     {'path': 'manage-users', 'title': 'User Management', component: ManageUserComponent},
     {'path': 'add-user', 'title': 'Add User', component: AddUserComponent},
     { path: 'manage-users/user-detail/:id', component: UserDetailComponent },
     // Route example: { path: 'add-student', title: 'Add', component: AddStudentComponent },
-
     {path:'addTeacher',component:AddTeacherComponent},
     {path:'listTeacher',component:ListTeacherComponent},
     {'path': 'staff', 'title': 'StaffList', component: StaffListComponent},
-
     //Route example {'path': 'add-student', 'title': 'add', component: AddStudentComponent},
 
 
