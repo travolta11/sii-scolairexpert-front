da08225370eba38de452279603788ed7723dd7c2
issam liqali <63265720+travolta11@users.noreply.github.com>
2024-08-12 10:32:53 +0100

    interface unauthorized

---
+++ src/app/components/unauthorized/unauthorized.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router'
+@Component({
+  selector: 'ssi-sx-unauthorized',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './unauthorized.component.html',
+  styleUrl: './unauthorized.component.scss'
+})
+export class UnauthorizedComponent {
+
+}
